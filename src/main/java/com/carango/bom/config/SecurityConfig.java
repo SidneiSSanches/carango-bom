58b3139ea71402b68488b0a494673cf6daef860e
wilia <wilnasc990s3@gmail.com>
2025-03-21 21:38:21 -0300

    Desabilitando configuração de headers Ãpara acesso ao h2 data base

--- src/main/java/com/carango/bom/config/SecurityConfig.java
+++ src/main/java/com/carango/bom/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,6 +32,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 						.csrf(AbstractHttpConfigurer::disable)
+						.headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
 						.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 						.authorizeHttpRequests(authz -> authz
 										.requestMatchers(
